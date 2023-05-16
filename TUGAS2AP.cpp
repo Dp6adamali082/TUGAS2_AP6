625411641922816165e975132c7b0061bf89c45c
dp6adamali082 <adamnasution2018@gmail.com>
2023-05-16 23:39:09 +0700

    TUGAS AP2 SAYA BANG

---
+++ TUGAS2AP.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main (){
+    system("CLS");
+        int nilai [3],petunjuk;
+        nilai[0] = 125;
+        nilai[1] = 345;
+        nilai[2] = 750;
+         petunjuk = nilai[0];
+    cout<<"Nilai"<< petunjuk << "ada di alamat memori"<< petunjuk   <<endl;
+    cout<<"Nilai"<< petunjuk << "ada di alamat memori"<< petunjuk+1 <<endl;    
+    cout<<"Nilai"<< petunjuk <<"ada di alamat memori"<< petunjuk+2 <<endl;
+    
+   
+}
